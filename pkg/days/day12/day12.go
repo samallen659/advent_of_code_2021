c69218b26b91447c3d5912874ea9abc8dd2aca6c
Sam Allen <samallen659@gmail.com>
2023-08-29 20:02:26 +0100

    adds solution to day 12 part 1

--- pkg/days/day12/day12.go
+++ pkg/days/day12/day12.go
@@ -1,47 +1,50 @@
 package day12
 
 import (
-	"fmt"
 	"github.com/samallen659/advent_of_code/2021/pkg/utils"
 	"golang.org/x/exp/slices"
 	"strings"
 	"unicode"
 )
 
-var input = utils.ReadInput("/inputs/day12/testInput.txt")
+var input = utils.ReadInput("/inputs/day12/input1.txt")
 var data = strings.Split(input, "\n")
 
 func Part1() int {
 	graph := buildGraph(data)
-	fmt.Println(graph)
-	var stack Stack
-	var visited []string
 	pathCount := 0
-	stack.push("start")
-	for stack.len > 0 {
-		cave, err := stack.pop()
-		if err != nil {
-			break
-		}
-		if cave == "end" {
+	visited := make(map[string]bool)
+	var dfs func(string)
+	dfs = (func(node string) {
+		if node == "end" {
 			pathCount++
-			continue
+			return
 		}
-		visited = append(visited, cave)
-		for _, connected := range graph[cave] {
-			if connected != "start" {
-				if unicode.IsLower([]rune(connected)[0]) && !slices.Contains(visited, connected) {
-					stack.push(connected)
-				} else {
-					stack.push(connected)
-				}
+		if isLower(node) {
+			visited[node] = true
+		}
+		for _, neighbor := range graph[node] {
+			if !visited[neighbor] {
+				dfs(neighbor)
 			}
 		}
+		if isLower(node) {
+			visited[node] = false
+		}
+	})
 
-		fmt.Println(visited)
-	}
+	dfs("start")
 
 	return pathCount
+}
+
+func isLower(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLower(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
 }
 
 func buildGraph(input []string) map[string][]string {
@@ -59,26 +62,4 @@ func buildGraph(input []string) map[string][]string {
 		}
 	}
 	return graph
-}
-
-type Stack struct {
-	data []string
-	len  int
-}
-
-func (s *Stack) push(d string) {
-	s.data = append(s.data, d)
-	s.len++
-}
-
-func (s *Stack) pop() (string, error) {
-	if s.len == 0 {
-		return "", fmt.Errorf("Empty Stack")
-	}
-
-	val := s.data[s.len-1]
-	s.data = append(s.data[:s.len-1], s.data[s.len:]...)
-	s.len--
-
-	return val, nil
 }
